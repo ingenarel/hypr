774358503588175493165b0fadf7776c78277631
ingenarel <ingenarelitems@gmail.com>
2024-12-31 21:13:32 +0600

    changed dirs for notes

--- notes/notes.sh
+++ notes/notes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    inotifywait -e modify "$HOME/notes.txt"
-    magick -font "/usr/share/fonts/TTF/HackNerdFont-Regular.ttf" -background "#110015" -fill "#FF1163" -size 1440x850 -pointsize 20 caption:"$(cat $HOME/notes.txt)" $HOME/notes.png &&\
-        swww img --resize fit -t none "$HOME/notes.png"
+    inotifywait -e modify "$HOME/.config/hypr/notes/notes.txt"
+    magick -font "/usr/share/fonts/TTF/HackNerdFont-Regular.ttf" -background "#110015" -fill "#FF1163" -size 1440x850 -pointsize 20 caption:"$(cat $HOME/.config/hypr/notes/notes.txt)" $HOME/.config/hypr/notes/notes.png &&\
+        swww img --resize fit -t none "$HOME/.config/hypr/notes/notes.png"
 done
