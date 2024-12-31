f1e94c647906351e28d6a9027a1d3e729f91b04a
ingenarel <ingenarelitems@gmail.com>
2025-01-01 05:22:31 +0600

    the script exits on failure

--- notes/notes.sh
+++ notes/notes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 swww img --resize fit -t none "$HOME/.config/hypr/notes/notes.png"
 while true; do
     inotifywait -e modify "$HOME/.config/hypr/notes/notes.txt"
