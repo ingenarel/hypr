e6d5081a0b661158fae81d5cc870a5f975c1b16d
ingenarel <ingenarelitems@gmail.com>
2025-01-01 03:44:48 +0600

    notes executes swww first

--- notes/notes.sh
+++ notes/notes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+swww img --resize fit -t none "$HOME/.config/hypr/notes/notes.png"
 while true; do
     inotifywait -e modify "$HOME/.config/hypr/notes/notes.txt"
     magick -font "/usr/share/fonts/TTF/HackNerdFont-Regular.ttf" -background "#110015" -fill "#FF1163" -size 1440x850 -pointsize 20 caption:"$(cat $HOME/.config/hypr/notes/notes.txt)" $HOME/.config/hypr/notes/notes.png &&\
