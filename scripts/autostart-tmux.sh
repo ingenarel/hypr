93c63f0e8cc2eec7c692971751025aac96457810
ingenarel <ingenarelitems@gmail.com>
2025-01-28 15:38:14 +0600

    (feat): fastfetch logo padding now dynamic

--- scripts/autostart-tmux.sh
+++ scripts/autostart-tmux.sh
@@ -2,5 +2,5 @@
 if [[ $(tmux list-sessions) ]]; then
     tmux attach
 else
-    tmux new-session "fastfetch --logo-padding-left 70; $SHELL"
+    tmux new-session "fastfetch --logo-padding-left \$(( ( \$(tput cols) - 93) / 2 )); \$SHELL"
 fi
