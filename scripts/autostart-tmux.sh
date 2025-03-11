66128259612b49cf7e0cd3853fcde3b21deaa110
ingenarel <ingenarelitems@gmail.com>
2025-03-11 22:32:37 +0600

    (scripts): tmux doesn't open with fastfetch now

--- scripts/autostart-tmux.sh
+++ scripts/autostart-tmux.sh
@@ -2,5 +2,6 @@
 if [[ $(tmux list-sessions) ]]; then
     tmux attach
 else
-    tmux new-session "fastfetch --logo-padding-left \$(( ( \$(tput cols) - 93) / 2 )); \$SHELL"
+    # tmux new-session "fastfetch --logo-padding-left \$(( ( \$(tput cols) - 93) / 2 )); \$SHELL"
+    tmux
 fi
